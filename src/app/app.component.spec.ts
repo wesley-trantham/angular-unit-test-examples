7139acd6e9b8203471c345d78b52eafed63d4730
Wesley Trantham <wes.trantham@gmail.com>
2019-08-25 15:06:40 -0500

    skipping tests

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,73 +1,75 @@
 import { Contact } from './models/contact.model';
 
-describe('AppComponent', () => {
-  it('should add numbers', () => {
-    // Arrange
-    let x = 0;
-    // Act
-    x += 1;
-    // Assert
-    expect(x).toBe(1);
-  });
-});
-
-describe('demo', () => {
-  beforeAll(() => {
-    console.log('outer beforeAll');
-  });
-  afterAll(() => {
-    console.log('outer afterAll');
-  });
-  beforeEach(() => {
-    console.log('outer beforeEach');
-  });
-  afterEach(() => {
-    console.log('outer afterEach');
+xdescribe('jasmine tests', () => {
+  describe('AppComponent', () => {
+    it('should add numbers', () => {
+      // Arrange
+      let x = 0;
+      // Act
+      x += 1;
+      // Assert
+      expect(x).toBe(1);
+    });
   });
 
-  it('compares strings', () => {
-    console.log('running outer it');
-    expect('string').toBe('string');
-  });
-
-  describe('inner demo', () => {
+  describe('demo', () => {
+    beforeAll(() => {
+      console.log('outer beforeAll');
+    });
+    afterAll(() => {
+      console.log('outer afterAll');
+    });
     beforeEach(() => {
-      console.log('inner beforeEach');
+      console.log('outer beforeEach');
     });
     afterEach(() => {
-      console.log('inner afterEach');
+      console.log('outer afterEach');
     });
 
-    it('should add numbers', () => {
-      console.log('starting inner test 1');
-      const x = 1 + 1;
-      expect(x).toBe(2);
+    it('compares strings', () => {
+      console.log('running outer it');
+      expect('string').toBe('string');
     });
 
-    it('should add more numbers', () => {
-      console.log('starting inner test 2');
-      const x = 2 + 2;
-      expect(x).toBe(4);
+    describe('inner demo', () => {
+      beforeEach(() => {
+        console.log('inner beforeEach');
+      });
+      afterEach(() => {
+        console.log('inner afterEach');
+      });
+
+      it('should add numbers', () => {
+        console.log('starting inner test 1');
+        const x = 1 + 1;
+        expect(x).toBe(2);
+      });
+
+      it('should add more numbers', () => {
+        console.log('starting inner test 2');
+        const x = 2 + 2;
+        expect(x).toBe(4);
+      });
     });
   });
-});
 
-describe('expects', () => {
-  it('should show expects', () => {
-    const a = new Contact();
-    const b = new Contact();
+  describe('expects', () => {
+    it('should show expects', () => {
+      const a = new Contact();
+      const b = new Contact();
 
-    a.firstName = 'Adam';
-    b.firstName = 'Adam';
+      a.firstName = 'Adam';
+      b.firstName = 'Adam';
 
-    expect(a).toBe(b); // fails
-    expect(a).toEqual(b); // succeeds!
-  });
+      expect(a).toBe(b); // fails
+      expect(a).toEqual(b); // succeeds!
+    });
 
-  it('handles truthy', () => {
-    const a = new Contact();
-    expect(a).toBeTruthy();
-    expect(true).toBe(true);
-    expect(true).toBeTruthy(); // avoid
+    it('handles truthy', () => {
+      const a = new Contact();
+      expect(a).toBeTruthy();
+      expect(true).toBe(true);
+      expect(true).toBeTruthy(); // avoid
+    });
   });
 });
